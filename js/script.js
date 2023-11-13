0c2f3cba3a48423cf5f0b105b0216a693cfe4816
jignaciomoya <joseignaciomoyajim@gmail.com>
2023-11-14 00:35:15 +0100

    adolf

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 import peliculas from './peliculas.js'
 
+let accion = [];
+let thriller = [];
+let aventura = [];
+
+const mostrarPeliculas = () =>{
+    for(let i = 0; i < peliculas.length; i++){
+        console.log(i);
+        if(peliculas[i].genre_ids.includes(28)){
+            accion.unshift(peliculas[i].original_title);
+        }
+        if(peliculas[i].genre_ids.includes(53)){
+            thriller.unshift(peliculas[i].original_title);
+        }
+        if(peliculas[i].genre_ids.includes(12)){
+            aventura.unshift(peliculas[i].original_title);
+        }
+    }
+}
+mostrarPeliculas();
+
+
+
